103bc5a4b66d795a789929b08ecf2e7c9ca74ce6
Henrique Ferraz <henriqueferraz@ofnet.com.br>
2017-05-04 17:29:17 -0300

    04/05 17:20

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/users', users)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found')
+  const err = new Error('Página não encontrada')
   err.status = 404
   next(err)
 })
