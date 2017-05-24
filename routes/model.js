6683f5b3745c45c25a0ce28590002047df4288bc
Henrique Ferraz <henriquefrerraz@ofnet.com.br>
2017-05-24 18:29:18 -0300

    24/05/2017 18:29

--- routes/model.js
+++ routes/model.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const nome = require('./../fields/fields-nome');
-const idade = require('./../fields/fields-idade');
+const email = require('./../fields/fields-email');
+const senha = require('./../fields/fields-senha');
 const created_id = require('./../fields/fields-create_id');
 
 // Criação do Schema
 
 const _schema = {
         nome,
-        idade,
+        email,
+        senha,
         created_id
 };
 
