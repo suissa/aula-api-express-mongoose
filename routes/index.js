1b9326ae01916bbf9e4de2779659c9cdfb0e397b
Henrique Ferraz <henriquefrerraz@ofnet.com.br>
2017-05-25 17:33:46 -0300

    25/05 17:33

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Expresso' });
+  res.render('index', { title: 'Área Administrativa' });
 });
 
 /* GET Pagina Suissa. */
