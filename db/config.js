d310f2a34563ebb2925113fa5d9ac766e6b6267c
Henrique Ferraz <henriquefrerraz@ofnet.com.br>
2017-05-26 18:41:13 -0300

    18:41

--- db/config.js
+++ db/config.js
@@ -4,24 +4,24 @@ const dbUrl = 'mongodb://henriqueferraz:lai7452@ds127101.mlab.com:27101/ofnet';
 mongoose.connect(dbUrl);
 
 mongoose.connection.on('connected', function () {
-    console.log('Mongoose default connection open to ' + dbUrl);
+    console.log('Mongoose -> conexão padrão aberta em: ' + dbUrl);
 });
 
 mongoose.connection.on('error', function (err) {
-    console.log('Mongoose default connection error ' + err);
+    console.log('Mongoose -> erro na conexão padrão: ' + err);
 });
 
 mongoose.connection.on('disconnected', function () {
-    console.log('Mongoose default connection disconnected ');
+    console.log('Mongoose -> conexão padrão disconectada ');
 });
 
 mongoose.connection.on('open', function () {
-    console.log('Mongoose default connection is open ');
+    console.log('Mongoose -> conexão padrao aberta! ');
 });
 
 process.on('SIGINT', function () {
     mongoose.connection.close(function () {
-        console.log('Mongoose default connection disconnected through app termination');
+        console.log('Mongoose -> conexão padrão desconectada por término do aplicativo');
         process.exit(0);
     });
 });
